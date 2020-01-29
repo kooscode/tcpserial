edecc206323e559226e60eaf1b029dff75252ce1
koos <koos@terraclearinc.com>
2020-01-28 18:13:52 -0800

    fixed contributor

--- src/tcpserial.cpp
+++ src/tcpserial.cpp
@@ -1,6 +1,6 @@
 /*
  * Simple application to proxy Serial Port to TCP Server 
- * Copyright (C) 2017 TerraClear, Inc.
+ * Copyright (C) 2017 Koos du Preez
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
